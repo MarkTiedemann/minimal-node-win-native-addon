7eb852a2f6424fc22bcd934d4b35feae584ab859
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-03-29 22:44:09 +0100

    Update example code

--- addon.cc
+++ addon.cc
@@ -1,25 +1,27 @@
+#define NAPI_VERSION 1
 #include <string>
 #include <windows.h>
 #include "include\node_api.h"
+#define MAX_LENGTH MAX_COMPUTERNAME_LENGTH + 1
 
-napi_value init(napi_env env, napi_value exp)
+napi_value init(napi_env env, napi_value exports)
 {
-  TCHAR tch[MAX_COMPUTERNAME_LENGTH + 1];
-  DWORD dw = sizeof(tch) / sizeof(tch[0]);
-  GetComputerName(tch, &dw);
+  // Get computer name
+  TCHAR name_tchar[MAX_LENGTH];
+  DWORD name_length = MAX_LENGTH;
+  GetComputerName(name_tchar, &name_length);
 
-#ifdef UNICODE
-  std::wstring wstr = tch;
-  std::string str = std::string(wstr.begin(), wstr.end());
-#else
-  std::string str = tch;
-#endif
+  // Convert to string
+  std::string name_str = name_tchar;
 
-  napi_value nv;
-  napi_create_string_utf8(env, str.c_str(), NAPI_AUTO_LENGTH, &nv);
-  napi_set_named_property(env, exp, "computer_name", nv);
+  // Convert to napi value
+  napi_value napi_str;
+  napi_create_string_utf8(env, name_str.c_str(), name_str.length(), &napi_str);
 
-  return exp;
+  // Export computer name
+  napi_set_named_property(env, exports, "computer_name", napi_str);
+
+  return exports;
 }
 
 NAPI_MODULE(addon, init)
